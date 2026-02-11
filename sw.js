f4b26ec3e4034533c8859d0e9b9170bf1268dcd0
MattiaPigna <mattiapignatiel@gmail.com>
2026-02-11 18:48:22 +0100

    feat: Enhance PWA and app metadata
    Configures the application for better PWA integration by updating the manifest file with correct naming, icons, and start URL. Also, enhances the HTML meta tags for mobile web app experience and updates the service worker to properly handle caching and activation.

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+
 const CACHE_NAME = 'stork-fantasy-v1';
 const ASSETS_TO_CACHE = [
   '/',
@@ -12,6 +13,22 @@ self.addEventListener('install', (event) => {
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
